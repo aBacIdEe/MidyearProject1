4198732e007563c3ed4784fdc0cf9b4c0adfbe58
Zenoson <zenoson1234@gmail.com>
2022-01-31 12:15:44 -0500

    Set basic backbone of timer

--- Timer.py
+++ Timer.py
@@ -1,1 +1,18 @@
 import time
+
+class Timer():
+    def __init__(self, time):
+        #time is given as number of seconds
+        self.time = time
+
+    def pause_timer(self):
+        # TODO
+        pass
+
+    def resume_timer(self):
+        # TODO
+        pass
+
+    def update_timer(self):
+        # TODO
+        pass
