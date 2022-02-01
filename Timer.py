3267ac6c379f01a136dc50b8975a6bb74c91c98c
Zenoson <zenoson1234@gmail.com>
2022-02-01 12:09:04 -0500

    made it possible to convert seconds into hh:mm:ss

--- Timer.py
+++ Timer.py
@@ -3,8 +3,20 @@ import time
 class Timer():
     def __init__(self, time, increment):
         #time is given as number of seconds
-        self.time = time
+        self.time = time #should be less than a day
         self.increment = increment
+
+    def show_time(self):
+        minutes = 0
+        hours = 0
+        for min in range(0, self.time, 60):
+            minutes += 1
+        for hour in range(0, minutes, 60):
+            hours += 1
+        
+        minutes = minutes % 60
+        seconds = self.time % 60
+        return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
 
     def pause_timer(self):
         self.update_timer(False)
