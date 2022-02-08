49efe6c785995e9d7ace0cbef6116bd38429fee5
Zenoson <zenoson1234@gmail.com>
2022-02-08 11:52:59 -0500

    Backbone for timer gui

--- TimerGUI.py
+++ TimerGUI.py
@@ -1,3 +1,17 @@
 import Timer as tm
 import time
 import tkinter
+
+class Application(tkinter.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.grid()
+        self.create_widgets()
+
+    def create_widgets(self):
+        pass
+
+root = tkinter.Tk
+root.title("Timer GUI")
+app = Application(root)
+root.mainloop()
